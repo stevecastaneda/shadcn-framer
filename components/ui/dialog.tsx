227c83249c1646001f193e73b77f1709d3cde626
Steve <steve.castaneda@me.com>
2023-05-30 15:35:32 -0500

    Pass classnames to animated motion.div

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -37,9 +37,9 @@ const DialogOverlay = React.forwardRef<
       transition: { duration: 0.1 },
     }}
     transition={{ duration: 0.2 }}
-    className="fixed inset-0 z-50 bg-background/80"
+    className={cn("fixed inset-0 z-50 bg-background/80", className)}
   >
-    <DialogPrimitive.Overlay ref={ref} className={cn(className)} {...props} />
+    <DialogPrimitive.Overlay ref={ref} {...props} />
   </motion.div>
 ));
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
@@ -59,13 +59,12 @@ const DialogContent = React.forwardRef<
           animate={{ opacity: 1, scale: 1, y: 0 }}
           exit={{ opacity: 0, scale: 0.9, transition: { duration: 0.1 } }}
           transition={{ duration: 0.2 }}
-          className="fixed z-50 grid w-full gap-4 rounded-b-lg border bg-background p-6 shadow-lg sm:max-w-lg sm:rounded-lg"
+          className={cn(
+            "fixed z-50 grid w-full gap-4 rounded-b-lg border bg-background p-6 shadow-lg sm:max-w-lg sm:rounded-lg",
+            className
+          )}
         >
-          <DialogPrimitive.Content
-            ref={ref}
-            className={cn(className)}
-            {...props}
-          >
+          <DialogPrimitive.Content ref={ref} {...props}>
             {children}
             <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
               <X className="h-4 w-4" />
