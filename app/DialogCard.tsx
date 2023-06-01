5f0b929dff4a04a885b929b9d3192166c7897322
Steve <steve.castaneda@me.com>
2023-06-01 08:10:40 -0500

    Fix import

--- app/DialogCard.tsx
+++ app/DialogCard.tsx
@@ -8,7 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog-motion-framer";
+} from "@/components/ui/dialog";
 
 export function DialogCard() {
   return (
